163fe355da0b47bd6e28fa193d57676bde0fcfa3
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2020-12-09 15:16:36 +0100

    Delete combat.py

--- combat.py
+++
@@ -1,4 +0,0 @@
-
-# # significations : nom, attaque mini, attaque max, défense, multiplicateur de dégat (arme), vie,pvmax, précision
-Prince_stats = ["Prince", 8, 12, 3, 1.3, 60, 90, 5, 1, 0, 15, 0, 1, 60, 15, 2]
-# significations : (0)nom, (1)attaque mini, (2)attaque max, (3)défense, (4)multiplicateur de dégat (arme), (5)vie, (6)précision, (7)chance de coup bas, (8)niveau de coup bas, (9)xp coup bas, (10)mana, (11)puissance magique, (12)attques magiques, (13) PV max, (14) MP max, (15) def mini
